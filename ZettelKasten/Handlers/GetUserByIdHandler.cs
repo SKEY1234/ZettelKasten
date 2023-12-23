2040deab390d868546d472607c2168e675604655
Стрелецкий Иван <streletskiyin@pik.ru>
2023-12-23 16:40:08 +0300

    GetUserById

---
+++ ZettelKasten/Handlers/GetUserByIdHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ZettelKasten.Models.API;
+using ZettelKasten.Models.DTO;
+using ZettelKasten.ORM;
+using ZettelKasten.Queries;
+
+namespace ZettelKasten.Handlers;
+
+public class GetUserByIdHandler : IRequestHandler<GetUserByIdQuery, Result<User>>
+{
+    private readonly ZettelkastenContext _context;
+    public GetUserByIdHandler(ZettelkastenContext context)
+    {
+        _context = context;
+    }
+    public async Task<Result<User>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+    {
+        User? user = await _context.Users.Where(u => u.UserId == request.UserId).FirstOrDefaultAsync();
+
+        if (user is null)
+            return Result<User>.Failure($"Не найден пользователь с Id {request.UserId}");
+
+        return Result<User>.Success(user);
+    }
+}
